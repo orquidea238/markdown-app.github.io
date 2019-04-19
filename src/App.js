15171b86f620aa699f64fe9e0e8e9a5d352ac8d7
Orquidea Marques <orquidea_238@hotmail.fr>
2019-04-19 23:07:05 +0200

    Mon appli

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import marked from 'marked'
+import { sampleText } from './sampleText';
 
 class App extends Component {
+  state = {
+    text: sampleText
+  }
+
+  componentDidMount () {
+    const text = localStorage.getItem('text')
+   
+    if (text) {
+      this.setState({ text })
+    } else { 
+      this.setState({ text: sampleText })
+    }
+  }
+
+  componentDidUpdate (){
+    const { text } = this.state
+    localStorage.setItem('text', text)
+  }
+
+  handleChange = event =>{
+    const text = event.target.value
+    this.setState({ text })
+  }
+
+  renderText = text =>{ 
+    const __html = marked(text, { sanitize: true })
+    return { __html };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea 
+            onChange={this.handleChange}
+            value={this.state.text}
+            className="form-control" 
+            row="35">
+            </textarea>
+          </div>
+          <div className="col-sm-6">
+            <div dangerouslySetInnerHTML={this.renderText(this.state.text)} />
+          </div>
+        </div>
       </div>
     );
   }
